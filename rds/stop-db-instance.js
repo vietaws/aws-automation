f70e1d8b2a7d686e332a735a8c3214fd55b5043f
Viet <viettq@3c0630014423.ant.amazon.com>
2022-11-28 14:19:12 +0700

    add rds automation

---
+++ rds/stop-db-instance.js
@@ -0,0 +1,30 @@
+import {
+  RDSClient,
+  StopDBClusterCommand,
+  StopDBInstanceCommand,
+} from '@aws-sdk/client-rds'; // ES Modules import
+
+const client = new RDSClient({
+  region: 'ap-southeast-1',
+});
+
+export const handler = async (event) => {
+  // TODO implement
+
+  const inputDBCluster = {
+    DBClusterIdentifier: 'aurora-sls-dev',
+  };
+  const inputDBInstance = {
+    DBInstanceIdentifier: 'aws-rds-mysql-dms-demo',
+  };
+
+  // const command = new StopDBClusterCommand(inputDBCluster);
+  const command = new StopDBInstanceCommand(inputDBInstance);
+  const response = await client.send(command);
+
+  const result = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return result;
+};
